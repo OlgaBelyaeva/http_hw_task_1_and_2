0145078a909f1fdb8a16459ecc9be9d129beeb4f
Ольга Беляева <olga310384@yandex.ru>
2022-06-16 20:38:24 +0700

    first commit

---
+++ http_hw_task1.py
@@ -0,0 +1,15 @@
+# Работа с библиотекой requests, http-запросы
+# Задача 1
+
+import requests
+
+def moste_intelligense(heroes):
+    url = 'https://superheroapi.com/api/2619421814940190/search/'
+    heroes_list = []
+    for hero in heroes:
+        response = requests.get(url + hero)
+        heroes_list.append(#тут код, который у героя из json  достате значение intelligence)
+    moste_intelligense_hero = max(heroes_list, key=lambda x: int(x[1]))
+    print(f'Самый умный герой - {moste_intelligense_hero})
+
+moste_intelligense(['Hulk', 'Captain America', 'Thanos'])
